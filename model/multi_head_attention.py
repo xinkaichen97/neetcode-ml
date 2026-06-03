2c5ba702f32234fea2c12a1e25035ba88f391067
Xinkai Chen <xinkaichen97@gmail.com>
2026-06-03 17:58:13 -0500

    Implement Multi-headed self-attention (Attention & Transformers 2/3)

---
+++ model/multi_head_attention.py
@@ -0,0 +1,45 @@
+import torch
+import torch.nn as nn
+from torchtyping import TensorType
+
+class MultiHeadedSelfAttention(nn.Module):
+
+    def __init__(self, embedding_dim: int, attention_dim: int, num_heads: int):
+        super().__init__()
+        torch.manual_seed(0)
+        self.head_size = attention_dim // num_heads
+        self.heads = nn.ModuleList()
+        for _ in range(num_heads):
+            self.heads.append(self.SingleHeadAttention(embedding_dim, self.head_size))
+        self.out = nn.Linear(attention_dim, attention_dim, bias=False)
+
+    def forward(self, embedded: TensorType[float]) -> TensorType[float]:
+        outputs = []
+        for head in self.heads:
+            outputs.append(head(embedded))
+        concated = torch.cat(outputs, dim=2)
+        return torch.round(self.out(concated), decimals=4)
+
+    class SingleHeadAttention(nn.Module):
+        def __init__(self, embedding_dim: int, attention_dim: int):
+            super().__init__()
+            torch.manual_seed(0)
+            self.key_gen = nn.Linear(embedding_dim, attention_dim, bias=False)
+            self.query_gen = nn.Linear(embedding_dim, attention_dim, bias=False)
+            self.value_gen = nn.Linear(embedding_dim, attention_dim, bias=False)
+
+        def forward(self, embedded: TensorType[float]) -> TensorType[float]:
+            k = self.key_gen(embedded)
+            q = self.query_gen(embedded)
+            v = self.value_gen(embedded)
+
+            scores = q @ torch.transpose(k, 1, 2) # @ is the same as torch.matmul()
+            context_length, attention_dim = k.shape[1], k.shape[2]
+            scores = scores / (attention_dim ** 0.5)
+
+            lower_triangular = torch.tril(torch.ones(context_length, context_length))
+            mask = lower_triangular == 0
+            scores = scores.masked_fill(mask, float('-inf'))
+            scores = nn.functional.softmax(scores, dim = 2)
+
+            return scores @ v
